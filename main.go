f03f348e1b6690c58a474fbb776f425b70d1ca1d
Reza Mirjahanian <rmirjahanian@gmail.com>
2024-05-30 09:52:19 +0330

    Add docker-compose

--- main.go
+++ main.go
@@ -25,12 +25,13 @@ type config struct {
 	GIN_TRUSTED_PROXIES_LIST []string
 }
 
-func loadConfig() (*config, error) {
-	viper.SetConfigFile(".env")
+func loadConfig(envPath string) (*config, error) {
+	viper.SetConfigFile(envPath)
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
-		return nil, err
+		log.Println("Can't read config file: ", envPath)
+		log.Println("Loading config from environment variables...")
 	}
 	//todo check if the config is not nil and valid
 	cfg := &config{}
@@ -39,12 +40,16 @@ func loadConfig() (*config, error) {
 	cfg.GIN_SERVER_ADDRESS = viper.GetString("GIN_SERVER_ADDRESS")
 	cfg.GIN_TRUSTED_PROXIES_LIST = strings.Split(viper.GetString("GIN_TRUSTED_PROXIES_LIST"), ",")
 
+	if cfg.RPC_ENDPOINT == "" || cfg.RPC_RATE_LIMITER_NUMBER == 0 || cfg.GIN_SERVER_ADDRESS == "" || len(cfg.GIN_TRUSTED_PROXIES_LIST) == 0 {
+		return nil, errors.New("config is not valid")
+	}
+
 	return cfg, nil
 }
 
 func main() {
 	// Load .env file
-	cfg, err := loadConfig()
+	cfg, err := loadConfig(".env")
 	checkIfErr(err)
 
 	// Get RPC endpoint from environment variable
